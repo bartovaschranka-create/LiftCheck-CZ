b8d989ed017071688348f9909de0d9766f2a3efd
Bartoš Jan <Jan.Bartos@zeppelin.com>
2026-08-20 15:04:10 +0200

    Fix fault photo processing

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'liftcontrol-cz-v1-6-5-349';
-const APP_VERSION_URL = './index.html?v=5.349';
+const CACHE_NAME = 'liftcontrol-cz-v1-6-5-350';
+const APP_VERSION_URL = './index.html?v=5.350';
 const APP_SHELL = [
   './',
   './index.html',
