333140ae818f239a00f57a1584618011adeeb64b
Bartoš Jan <Jan.Bartos@zeppelin.com>
2026-08-21 08:32:27 +0200

    Fix fault place photos and print preview

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'liftcontrol-cz-v1-6-5-350';
-const APP_VERSION_URL = './index.html?v=5.350';
+const CACHE_NAME = 'liftcontrol-cz-v1-6-5-351';
+const APP_VERSION_URL = './index.html?v=5.351';
 const APP_SHELL = [
   './',
   './index.html',
